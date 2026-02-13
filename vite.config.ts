24c779fff0ea84dc3871f63fc623b1f54804e293
Shathrack V <sec23it169@sairamtap.edu.in>
2026-02-13 14:24:12 +0530

    Rename project to protoHQ-cryptacore_ai_services

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/llamacoder-project/",
+  base: "/protoHQ-cryptacore_ai_services/",
   plugins: [react()],
   resolve: {
     alias: {
